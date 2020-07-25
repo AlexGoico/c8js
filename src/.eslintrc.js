1efbf2ddfe3928203f8b54bf7f2ef90fee3b3e41
agoico <alexegoico@gmail.com>
2020-07-25 00:48:18 -0400

    Temporarily move eslint config to please github action

---
+++ src/.eslintrc.js
@@ -0,0 +1,39 @@
+module.exports = {
+  'env': {
+    'browser': true,
+    'es2020': true,
+    'node': true,
+  },
+  'extends': [
+    'google',
+  ],
+  'parserOptions': {
+    'ecmaVersion': 11,
+    'sourceType': 'module',
+  },
+  'plugins': [
+    'jsdoc',
+  ],
+  'rules': {
+    'require-jsdoc': 0,
+    'valid-jsdoc': 0,
+    'jsdoc/require-param': 2,
+    'jsdoc/require-param-type': 2,
+    'jsdoc/require-returns': 2,
+    'jsdoc/require-throws': 2,
+    'jsdoc/require-jsdoc': [
+      'error',
+      {
+        'publicOnly': true,
+        'require': {
+          'ArrowFunctionExpression': true,
+          'ClassDeclaration': true,
+          'ClassExpression': true,
+          'FunctionDeclaration': true,
+          'FunctionExpression': true,
+          'MethodDefinition': true,
+        },
+      },
+    ],
+  },
+};
