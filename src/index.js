a27f3577ba3fd9caba312d106126fb9d58e6a7b9
agoico <alexegoico@gmail.com>
2020-07-26 23:38:26 -0400

    Loads ROM into chip8 and fixes ROM loading

--- src/index.js
+++ src/index.js
@@ -2,33 +2,52 @@ import Chip8 from './Chip8';
 import PixiRenderer from './PixiRenderer';
 import * as ROMReader from './ROMReader';
 
-function setupROMSelector() {
-  // Reset rom selection each time page loads.
-  const romSelector = document.getElementsByTagName('input')[0];
-  if (romSelector) {
-    romSelector.value = '';
+class AppDriver {
+  static setupROMSelector() {
+    // Reset rom selection each time page loads.
+    const romSelector = document.getElementsByTagName('input')[0];
+    if (romSelector) {
+      romSelector.value = '';
 
-    romSelector.addEventListener('change', function handleROMSelection(event) {
-      const romFile = event.target.files[0];
-      if (romFile) {
-        ROMReader.read(romFile).then((rom) => {
-          console.log('Sequence of two byte instructions');
-          console.log(rom.toString());
+      romSelector.addEventListener('change',
+        function handleROMSelection(event) {
+          const romFile = event.target.files[0];
+          if (romFile) {
+            ROMReader.read(romFile).then((rom) => {
+              const romData = rom.toString();
+              console.log(`${rom.name}'s Opcodes`);
+              console.log(romData);
+
+              AppDriver.c8.loadROM(rom);
+              let memROMData = [];
+              for (let i = 0; i < rom.size(); i += 2) {
+                const msByte = AppDriver.c8.mem[0x200+i] << 8;
+                const lsByte = AppDriver.c8.mem[0x200+i+1];
+
+                const twoByte = msByte + lsByte;
+                memROMData.push(twoByte.toString(16));
+              }
+              memROMData = memROMData.map((byte) => `0x${byte}`).join(' ');
+              console.assert(memROMData === romData,
+                `ROM not correctly loaded into Chip8 memory`);
+            });
+          }
         });
-      }
-    });
-  } else {
-    console.error('Unable to find file selector field');
+    } else {
+      console.error('Unable to find file selector field');
+    }
+  }
+
+  static main() {
+    AppDriver.setupROMSelector();
+
+    const container = document.getElementById('emulator');
+    const renderer = new PixiRenderer(container, 500, 500);
+
+    AppDriver.c8 = new Chip8(renderer);
   }
 }
 
-function main() {
-  setupROMSelector();
+AppDriver.c8 = null;
 
-  const container = document.getElementById('emulator');
-  const renderer = new PixiRenderer(container, 500, 500);
-
-  new Chip8(renderer);
-}
-
-window.addEventListener('load', main);
+window.addEventListener('load', AppDriver.main);
