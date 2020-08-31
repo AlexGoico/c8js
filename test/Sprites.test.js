b774a710dfa0a886eae89d1b548414d652883538
BinBin <hebinsquared@gmail.com>
2020-08-31 03:57:40 -0500

    Refactor: Fixed eslint complaints about 4 char spacing instead of 2 for sprites.js and sprites.test.js

--- test/Sprites.test.js
+++ test/Sprites.test.js
@@ -1,17 +1,17 @@
 import {sprite} from '../src/constants/Sprites.js';
 
 describe('Constants - Sprites', function Sprites() {
-    test('sprite constant size to equal 16 sprites', function() {
-        expect(sprite.size).toEqual(16);
-    });
+  test('sprite constant size to equal 16 sprites', function() {
+    expect(sprite.size).toEqual(16);
+  });
 
-    test('character 0 sprite matches hexademical', function() {
-        const zeroHex = '0xF0, 0x90, 0x90, 0x90, 0xF0';
-        expect(sprite.get(0)).toEqual(zeroHex);
-    });
+  test('character 0 sprite matches hexademical', function() {
+    const zeroHex = '0xF0, 0x90, 0x90, 0x90, 0xF0';
+    expect(sprite.get(0)).toEqual(zeroHex);
+  });
 
-    test('character F sprite matches hexademical', function() {
-        const fHex = '0xF0, 0x80, 0xF0, 0x80, 0x80';
-        expect(sprite.get('F')).toEqual(fHex);
-    })
-})
+  test('character F sprite matches hexademical', function() {
+    const fHex = '0xF0, 0x80, 0xF0, 0x80, 0x80';
+    expect(sprite.get('F')).toEqual(fHex);
+  });
+});
